5a9af71e0edb95c70c244f833cc30fe5d6dd87a1
AKASH SINGH <101030179+akkivanguu@users.noreply.github.com>
2022-04-28 11:04:37 +0530

    Add files via upload

---
+++ 3_Implementation(code)/inc/usart.h
@@ -0,0 +1,17 @@
+
+#ifndef usart_H
+#define usart_H
+#define __AVR_ATmega32U4__
+#define FOSC_FOR_USART        16000000UL
+#define BAUD_RATE     9600UL
+#define BAUD_VAL      FOSC_FOR_USART/16/BAUD_RATE-1
+
+    
+
+void USART_Tx_byte(char* c );
+
+void usart_init(unsigned int );
+
+void USART_Tx_string( char*); 
+
+#endif
